a69e00405d295f9b296211610b817958f4305a7b
nmhummel <natalie.hummel@gmail.com>
2021-01-06 09:50:20 -0600

    commit tree 08f8df51e46b9b47ea4c8f2a13b8abf67dc07ef1

--- app/controllers/authors_controller.rb
+++ app/controllers/authors_controller.rb
@@ -1,15 +1,22 @@
 class AuthorsController < ApplicationController
+  
+
   def show
     @author = Author.find(params[:id])
   end
 
   def new
+    @author = Author.new
   end
 
   def create
-    @author = Author.create!(author_params)
-
-    redirect_to author_path(@author)
+    @author = Author.new(author_params)
+    if @author.valid?
+      @author.save
+      redirect_to author_path(@author)
+    else 
+      render :new
+    end
   end
 
   private
